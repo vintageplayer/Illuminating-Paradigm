51a40a5fe7a56a12c2838e1dba792c746c2db736
Aditya Agarwal <adityag2511@gmail.com>
2017-03-11 22:31:43 +0530

    Setting up

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#ifdef __APPLE__
+	#define SYS_OS 1
+#elif defined __unix__
+	#define SYS_OS 2
+#elif defined __CYGWIN__
+	#ifdef _WIN32 || defined _WIN64
+		#define SYS_OS 3
+	#define SYS_OS 2
+#else
+	#define 0
+#endif
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc,char** argv)
+{
+
+	cout<<endl;
+	return 0;
+}
